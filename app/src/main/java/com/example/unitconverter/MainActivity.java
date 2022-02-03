255da9c33b9e80edbd9c9a9accf8d38f6f5be865
Md Shakil <shakil.eub.cse@gmail.com>
2022-02-03 12:18:13 +0600

    App Navigation

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.unitconverter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState == null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new AboutUs()).commit();
+        navigationView.setCheckedItem(R.id.nav_aboutus);}
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case  R.id.nav_aboutus:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutUs()).commit();
+                break;
+            case  R.id.nav_email:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Email()).commit();
+                break;
+
+
+        }
+        return true;
+    }
+}
